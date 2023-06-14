a2f325d7dfba96ad517802298fd39878299234c0
David McIntyre <davidrobert.mcintyre@gmail.com>
2023-06-14 14:22:17 +0200

    updated tests

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -23,6 +23,7 @@ class CitySearch extends Component {
         this.setState({
           query: suggestion
         });
+        this.props.updateEvents(suggestion);
       }
 
   render() {
@@ -41,8 +42,8 @@ class CitySearch extends Component {
                 onClick={() => this.handleItemClicked(suggestion)}>
                 {suggestion}</li>
                 ))}
-            <li>
-             <b>See all cities</b>
+            <li onClick={() => this.handleItemClicked("all")}>
+                <b>See all cities</b>
             </li>
             </ul>
       </div>
