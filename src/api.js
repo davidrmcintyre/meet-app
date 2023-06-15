6df2c229b784d3b6030a9b70a38bb227a17eb138
David McIntyre <davidrobert.mcintyre@gmail.com>
2023-06-15 15:45:04 +0200

    bug fixes

--- src/api.js
+++ src/api.js
@@ -67,6 +67,16 @@ export const extractLocations = (events) => {
   return accessToken;
 }
 
+const checkToken = async (accessToken) => {
+    const result = await fetch(
+      `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+    )
+      .then((res) => res.json())
+      .catch((error) => error.json());
+  
+    return result;
+  };
+
     const getToken = async (code) => {
     try {
         const encodeCode = encodeURIComponent(code);
